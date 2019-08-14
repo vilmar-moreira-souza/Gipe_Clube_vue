e4adf4e8473b10bdbf552c428ff60dfc49b359bc
vilmar <vilmarmoreira2010@gmail.com>
2019-08-14 19:31:30 -0300

    add vue
    add sincronia com vue

---
+++ js/funcoes.js
@@ -0,0 +1,36 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    errors: [],
+    name: null,
+    email: null,
+    mensagem: null
+  },
+  methods: {
+    checkForm: function (e) {
+      this.errors = [];
+
+      if (!this.name) {
+        this.errors.push('O nome é obrigatório.');
+      }
+      if (!this.mensagem) {
+        this.errors.push('A mensagem é obrigatória.');
+      }
+      if (!this.email) {
+        this.errors.push('O e-mail é obrigatório.');
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push('Utilize um e-mail válido.');
+      }
+
+      if (!this.errors.length) {
+        return true;
+      }
+
+      e.preventDefault();
+    },
+    validEmail: function (email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+  }
+})
